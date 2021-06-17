c79639a8d8a1ed42931384558afa6af22a158294
maryakinyi <79200051+maryakinyi@users.noreply.github.com>
2021-06-17 12:15:34 +0300

    Add files via upload

--- python_test.py
+++ python_test.py
@@ -6,26 +6,29 @@ for y in x:
 
 def divisible_by_three(n):
        c=range(n+1)
-       d=[]
+       #d=[]
        for u in c:
         if u%3==0:
-            d.append(u)
-            print(d)
+            #d.append(u)
+            print(u)
+divisible_by_three(20)
 
 def  greet():
         students = [{"age": 19, "name": "Eunice"}, {"age": 21, "name": "Agnes"}, {"age": 18, "name": "Teresa"}, {"age": 22, "name": "Asha"}]
-        for keys in students:
-                print(f"Hello, ")
-def list():
+        for student in students:
+                print(f"Hello,{student['name']} you were birn {2021-student['age']}")
+def list_Flatten():
         x = ['a','b','a','e','d','b','c','e','f','g','h']
-        y=x.remove("b")
+        y=set(x)
         print(y)
+list_Flatten()
 def divisible_by_7():
         c=[]
         for number in range(100,200):
                 if number%7==0:
                         c.append(number)
                         print(c)
+divisible_by_7()
 class Rectangle:
         def __ini__(self,width,length):
                 self.width=width
